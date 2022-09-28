1f1f06c1f1f1818ff369429c5951800985dc8220
Your Name <you@example.com>
2022-09-28 09:24:19 +0200

    first

---
+++ script.js
@@ -0,0 +1,425 @@
+const quizData = [
+    {
+        question: "Který je první den v týdnu?",
+        a: "sobota",
+        b: "středa",
+        c: "uterý",
+        d: "pondělí",
+        correct: "d",
+    },
+    {
+        question: "Kolik dní má týden?",
+        a: "7",
+        b: "6",
+        c: "12",
+        d: "30",
+        correct: "a",
+    },
+    {
+        question: "Kolik měsíců má rok?",
+        a: "7",
+        b: "20",
+        c: "12",
+        d: "24",
+        correct: "c",
+    },
+    {
+        question: "Kolik hodin má jeden den?",
+        a: "7",
+        b: "24",
+        c: "12",
+        d: "10",
+        correct: "b",
+    },
+    {
+      question: "Kolik je 5.5?",
+      a: "20",
+      b: "24",
+      c: "25",
+      d: "28",
+      correct: "c",
+  },{
+    question: "V kterém státu se mluví anglickým jazykem?",
+    a: "Kanada",
+    b: "Německo",
+    c: "Japonsko",
+    d: "Slovensko",
+    correct: "a",
+  },{
+    question: "Kde je Socha slobody?",
+    a: "v Americe",
+    b: "v Ázii",
+    c: "v Africe",
+    d: "v Austrálii",
+    correct: "a",
+  },{
+    question: "Jak se jmenuje hlavní město Německa?",
+    a: "Varšava",
+    b: "Mnichov",
+    c: "Amsterdam",
+    d: "Berlín",
+    correct: "d",
+  },{
+    question: "Který detektiv je Belgičan?",
+    a: "Derick",
+    b: "Poirot",
+    c: "Columbo",
+    d: "Big Ben",
+    correct: "b",
+  },{
+    question: "Jak se anglicky řekne jablko?",
+    a: "banana",
+    b: "pear",
+    c: "strawberry",
+    d: "apple",
+    correct: "d",
+  },{
+    question: "Jak se německy řekne čarodějnice?",
+    a: "ježibaba",
+    b: "witch",
+    c: "die Hexe",
+    d: "das Auto",
+    correct: "c",
+  },{
+    question: "Kolik hodin je na oběd?",
+    a: "10:00",
+    b: "11:00",
+    c: "12:00",
+    d: "20:00",
+    correct: "c",
+  },{
+    question: "Kolik hodin je o polnoci?",
+    a: "05:00",
+    b: "24:00",
+    c: "12:00",
+    d: "10:00",
+    correct: "b",
+  },{
+    question: "Které je hlavní město USA?",
+    a: "New York",
+    b: "Los Angeles",
+    c: "Washington, D.C.",
+    d: "Londýn",
+    correct: "c",
+  },{
+    question: "Kto chtěl sníst Honzíka a Mařenku?",
+    a: "vlk",
+    b: "myslivec",
+    c: "štístko",
+    d: "ježibaba",
+    correct: "d",
+  },{
+    question: "V kolika letech se staneme dospělými?",
+    a: "15",
+    b: "18",
+    c: "21",
+    d: "12",
+    correct: "b",
+  },{
+    question: "Která světová strana je oproti východu?",
+    a: "západ",
+    b: "jihozápad",
+    c: "jih",
+    d: "sever",
+    correct: "a",
+  },{
+    question: "Co vznikne smíchaním modré a žluté?",
+    a: "zelená",
+    b: "růžová",
+    c: "černá",
+    d: "fialová",
+    correct: "a",
+  },{
+    question: "Kolik je ročních období?",
+    a: "2",
+    b: "4",
+    c: "10",
+    d: "12",
+    correct: "b",
+  },{
+    question: "Hlavní město Španělska je:",
+    a: "Berlín",
+    b: "Moskva",
+    c: "Madrid",
+    d: "Bratislava",
+    correct: "c",
+  },{
+    question: "Ve ktorém měsíci jsou Vánoce?",
+    a: "v lednu",
+    b: "v únoru",
+    c: "v září",
+    d: "v prosinci",
+    correct: "d",
+  },{
+    question: "Jak se jmenuje český prezident?",
+    a: "Zuzana Čaputová",
+    b: "Jožko Mrkvička",
+    c: "Miloš Zeman",
+    d: "Márie Terezie",
+    correct: "c",
+  },{
+    question: "Z čeho se vyrábí papír?",
+    a: "ze dřeva",
+    b: "ze skla",
+    c: "z hlíny",
+    d: "z mouky",
+    correct: "a",
+  },{
+    question: "Jak se jmenuje mládě slepice?",
+    a: "kůzlátko",
+    b: "kuřátko",
+    c: "jehňátko",
+    d: "kačátko",
+    correct: "b",
+  },{
+    question: "Čím lidé dýchají?",
+    a: "plícemi",
+    b: "srdcem",
+    c: "rukami",
+    d: "vlasami",
+    correct: "a",
+  },{
+    question: "Čím zapisujeme hudbu?",
+    a: "písmeny",
+    b: "čísly",
+    c: "v notách",
+    d: "azbukou",
+    correct: "c",
+  },{
+    question: "Jaké chromozómy mají chlapci?",
+    a: "XY",
+    b: "XX",
+    c: "XXX",
+    d: "XZ",
+    correct: "a",
+  },{
+    question: "Jaká vysoká teplota je horúčka?",
+    a: "0°C",
+    b: "38°C",
+    c: "100°C",
+    d: "-100°C",
+    correct: "b",
+  },{
+    question: "Kolik hodin je na oběd?",
+    a: "12:00",
+    b: "08:00",
+    c: "18:00",
+    d: "24:00",
+    correct: "a",
+  },{
+    question: "Jak říkáme sestře mámy nebo táty?",
+    a: "ježibaba",
+    b: "děda",
+    c: "princezná",
+    d: "teta",
+    correct: "d",
+  },{
+    question: "Který měsíc je zimní?",
+    a: "červen",
+    b: "červenec",
+    c: "září",
+    d: "leden",
+    correct: "d",
+  },{
+    question: "Jak se řekne dýně anglicky?",
+    a: "tekvica",
+    b: "pumpkin",
+    c: "car",
+    d: "baby",
+    correct: "b",
+  },{
+    question: "Kolik je 1+2+3+4+5?",
+    a: "14",
+    b: "15",
+    c: "16",
+    d: "17",
+    correct: "b",
+  },{
+    question: "Jak vypadá beruška?",
+    a: "bílá s červenými tečkami",
+    b: "červená s černými tečkami",
+    c: "růžová",
+    d: "černobílá s proužkami",
+    correct: "b",
+  },{
+    question: "Kdy má svátek Mikuláš?",
+    a: "v květnu",
+    b: "v září",
+    c: "v prosinci",
+    d: "v srpnu",
+    correct: "c",
+  },{
+    question: "Kde je ostrov Madagaskar?",
+    a: "v okolí Austrálie",
+    b: "při Africe",
+    c: "na jižním pólu",
+    d: "vedle Grónska",
+    correct: "b",
+  },{
+    question: "Jakým jazykem mluví Rakušané?",
+    a: "slovenským",
+    b: "rakouzským",
+    c: "německým",
+    d: "italským",
+    correct: "c",
+  },{
+    question: "Kto je slovenský minister financí?",
+    a: "Alžbeta II.",
+    b: "Václav Havel",
+    c: "Božena Němcová",
+    d: "Igor Matovič",
+    correct: "d",
+  },{
+    question: "Co je opakem bílé?",
+    a: "červená",
+    b: "modrá",
+    c: "černá",
+    d: "zlatá",
+    correct: "c",
+  },{
+    question: "Vyber jenom pární čísla:",
+    a: "2,4,5",
+    b: "3,4,5",
+    c: "2,7,9",
+    d: "8,10,12",
+    correct: "d",
+  },
+  ];
+  
+  const quiz = document.getElementById('quiz')
+  const answerEls = document.querySelectorAll('.answer')
+  const questionEl = document.getElementById('question')
+  const result = document.getElementById('resultquestion')
+  const a_text = document.getElementById('a_text')
+  const b_text = document.getElementById('b_text')
+  const c_text = document.getElementById('c_text')
+  const d_text = document.getElementById('d_text')
+  const submitBtn = document.getElementById('submit')
+  
+  let currentQuiz = 0
+  let score = 0
+  
+  loadQuiz()
+  
+  function loadQuiz() {
+    deselectAnswers()
+  
+    const currentQuizData = quizData[currentQuiz]
+  
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+  }
+  
+  function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+  }
+  
+  function getSelected() {
+    let answer
+  
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+  
+    return answer
+  }
+  let answers= []; 
+  submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) { answers.push(answer);
+            console.log(answers);}
+  
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+            
+            
+        }
+        
+        currentQuiz++;
+        
+  
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            
+            
+  /* 
+  */
+           let results= quizData.map ((question,i) => 
+           
+           
+           ` 
+                <div class="${quizData[i].correct===answers[i]?
+                    "correct": "false" } quiz-header ">
+                <h2 id="question">${question.question}</h2>
+                <ul id="resultquestion"  >
+                  <li >
+                    <input type="radio" name="answer" id="a" class="answer">
+                    <label for="a" id="a_text">${question.a}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="b" class="answer">
+                    <label for="b" id="b_text">${question.b}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="c" class="answer">
+                    <label for="c" id="c_text">${question.c}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="d" class="answer">
+                    <label for="d" id="d_text">${question.d}</label>
+                  </li>
+                  <li>
+                  <h4>Vybral jsi: ${question[quizData[i].correct]}</h4>
+                  <h4>Správná odpověď: ${question[answers[i]]}</h4>
+  
+                
+                  </li>
+                  
+                </ul>
+              </div>`
+                
+                )
+  
+  //                 result.classList.add("correct")
+  // /* 
+                
+  
+                /* quizData.forEach ((question,i) =>
+                 quizData[i].correct===answers[i]?
+                  result.classList.add("correct")  : result.classList.add("false") 
+                 ) */
+            
+         
+  
+  
+            quiz.innerHTML = `
+                <h2>Získal jsi ${score} bodů z${quizData.length}.</h2>
+               
+  
+                
+                
+               
+    
+                ${results}
+  
+  
+                
+  
+                <button onclick="location.reload()">Znovu zkusit</button>
+            `
+        
+        
+        }
+    }
+  })
